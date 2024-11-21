a68df709418d21d42b2adbe98db6a286e4f9aaed
DinhDuy <dinhduy136157@gmail.com>
2024-11-21 22:35:07 +0700

    BIG UPDATE

--- DoAn/ComputerStore/Areas/Admin/Controllers/HomeController.cs
+++ DoAn/ComputerStore/Areas/Admin/Controllers/HomeController.cs
@@ -3,15 +3,76 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using ComputerStore.Models;
 
 namespace ComputerStore.Areas.Admin.Controllers
 {
     public class HomeController : Controller
     {
-        // GET: Admin/Home
+        private ComputerStoreEntities db = new ComputerStoreEntities();
+        public class DashboardViewModel
+        {
+            public List<StatusCount> StatusCounts { get; set; }
+            public List<CategoryCount> CategoryCounts { get; set; }
+            public int totalStatusOrderCount { get; set; }
+            public int totalCategoryOrderCount { get; set; }
+
+        }
+
+        public class StatusCount
+        {
+            public string Status { get; set; }
+            public int OrderCount { get; set; }
+        }
+
+        public class CategoryCount
+        {
+            public string CategoryName { get; set; }
+            public int OrderCount { get; set; }
+        }
+
+
         public ActionResult Index()
         {
-            return View();
+            // Truy vấn thống kê theo Status
+            var statusCounts = db.OrderItems
+                .Where(o => o.Status == "Đang chờ xác nhận" ||
+                            o.Status == "Đã hủy" ||
+                            o.Status == "Giao hàng thành công")
+                .GroupBy(o => o.Status)
+                .Select(g => new StatusCount
+                {
+                    Status = g.Key,
+                    OrderCount = g.Count()
+                })
+                .ToList();
+
+            // Truy vấn thống kê theo Category
+            var categoryCounts = (from o in db.OrderItems
+                                  join p in db.Products on o.ProductID equals p.ProductID
+                                  join c in db.Categories on p.CategoryID equals c.CategoryID
+                                  where new[] { "Cây máy tính", "Màn hình", "Tai nghe", "Chuột" }.Contains(c.CategoryName)
+                                  group o by c.CategoryName into g
+                                  select new CategoryCount
+                                  {
+                                      CategoryName = g.Key,
+                                      OrderCount = g.Count()
+                                  }).ToList();
+            var totalStatusOrderCount = statusCounts.Sum(s => s.OrderCount);
+            var totalCategoryOrderCount = categoryCounts.Sum(s => s.OrderCount);
+            // Gán dữ liệu vào ViewModel
+            var viewModel = new DashboardViewModel
+            {
+                StatusCounts = statusCounts,
+                CategoryCounts = categoryCounts,
+                totalStatusOrderCount = totalStatusOrderCount,
+                totalCategoryOrderCount = totalCategoryOrderCount
+            };
+            
+
+            return View(viewModel);
         }
+
+
     }
 }
---
+++ DoAn/ComputerStore/Areas/User/Controllers/HomeController.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ComputerStore.Models;
+using System.Data.Entity;
+
+
+namespace ComputerStore.Areas.User.Controllers
+{
+    public class HomeController : Controller
+    {
+        private ComputerStoreEntities db = new ComputerStoreEntities();
+        public class ProductDetailDto
+        {
+            public ProductDetail Pd { get; set; }
+            public Product P { get; set; }
+        }
+
+        public class ProductDetailsViewModel
+        {
+            public List<ProductDetailDto> PcDetailsList { get; set; }
+            public List<ProductDetailDto> PhoneDetailsList { get; set; }
+            public List<ProductDetailDto> ScreenDetailsList { get; set; }
+        }
+
+
+        public ActionResult Index()
+        {
+            // Danh sách 10 PC từ top10PCIds
+            var top10PCIds = (from oi in db.OrderItems
+                              join p in db.Products on oi.ProductID equals p.ProductID
+                              where p.CategoryID == 1
+                              group oi by oi.ProductID into g
+                              orderby g.Sum(x => x.Quantity) descending
+                              select g.Key)
+                                   .Take(10)
+                                   .ToList();
+
+            // Lấy chi tiết sản phẩm từ ProductDetails với 10 ProductID này
+            var pcDetailsList = (from pd in db.ProductDetails
+                                 join p in db.Products on pd.ProductID equals p.ProductID
+                                 where top10PCIds.Contains(pd.ProductID)
+                                 select new ProductDetailDto
+                                 {
+                                     Pd = pd,
+                                     P = p
+                                 }).ToList();  // Trả về danh sách ProductDetailDto
+            // Danh sách 10 Tai nghe từ top10PhoneIds
+            var top10PhoneIds = (from oi in db.OrderItems
+                                 join p in db.Products on oi.ProductID equals p.ProductID
+                                 where p.CategoryID == 2
+                                 group oi by oi.ProductID into g
+                                 orderby g.Sum(x => x.Quantity) descending
+                                 select g.Key)
+                                   .Take(10)
+                                   .ToList();
+
+            // Lấy chi tiết sản phẩm từ top10PhoneIds với 10 ProductID này
+            var headphoneDetailsList = (from pd in db.ProductDetails
+                                        join p in db.Products on pd.ProductID equals p.ProductID
+                                        where top10PhoneIds.Contains(pd.ProductID)
+                                        select new ProductDetailDto
+                                        {
+                                            Pd = pd,
+                                            P = p
+                                        }).ToList();  // Trả về danh sách ProductDetailDto
+
+            // Danh sách 10 Màn hình từ top10Screen
+            var top10Screen = (from oi in db.OrderItems
+                               join p in db.Products on oi.ProductID equals p.ProductID
+                               where p.CategoryID == 3
+                               group oi by oi.ProductID into g
+                               orderby g.Sum(x => x.Quantity) descending
+                               select g.Key)
+                                   .Take(10)
+                                   .ToList();
+
+            // Lấy chi tiết sản phẩm từ top10Screen với 10 ProductID này
+            var screenDetailsList = (from pd in db.ProductDetails
+                                     join p in db.Products on pd.ProductID equals p.ProductID
+                                     where top10Screen.Contains(pd.ProductID)
+                                     select new ProductDetailDto
+                                     {
+                                         Pd = pd,
+                                         P = p
+                                     }).ToList();  // Trả về danh sách ProductDetailDto
+
+            // Tạo ViewModel và truyền các danh sách chi tiết vào
+            var viewModel = new ProductDetailsViewModel
+            {
+                PcDetailsList = pcDetailsList,
+                PhoneDetailsList = headphoneDetailsList,
+                ScreenDetailsList = screenDetailsList
+            };
+
+            // Trả về View với ViewModel
+            return View(viewModel);
+        }
+
+    }
+}
