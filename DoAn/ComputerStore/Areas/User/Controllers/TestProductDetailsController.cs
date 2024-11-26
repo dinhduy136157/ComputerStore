9ae143a3a7fb34b03260aeaedb867d247a84f3c3
nguyen duy <nguyenduy110803@gmail.com>
2024-11-26 15:17:26 +0700

    Fix mega menu, Add onClick menu to Layout, ... Please read NguyenHuuDuy.txt for more details :v

---
+++ DoAn/ComputerStore/Areas/User/Controllers/TestProductDetailsController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ComputerStore.Areas.User.Controllers
+{
+    public class TestProductDetailsController : Controller
+    {
+        // GET: User/TestProductDetails
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
